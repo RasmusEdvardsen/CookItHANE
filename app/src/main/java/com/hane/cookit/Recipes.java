afa15f9fff46f4c7aea9ad90595c8ade57a13a12
Rasmus Edvardsen <rasmus.edvardsen@hotmail.com>
2016-06-06 13:01:10 +0200

    Initial commit

---
+++ app/src/main/java/com/hane/cookit/Recipes.java
@@ -0,0 +1,14 @@
+package com.hane.cookit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Recipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+}
